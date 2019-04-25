83624719169306a621d3cd0bc08685c937f4d0d1
Travis Exline <t.exline22@gmail.com>
2019-04-25 17:21:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -23,6 +23,6 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  return 7 + ((43 * 23) / 18)
+  return num1 + ((num2 * num3) / num4)
   #Hint:  __ + (( __ * __ ) / __ )
 end
